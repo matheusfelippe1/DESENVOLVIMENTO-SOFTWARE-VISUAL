3c8a69b0f30f41b810eef04b3dfae0e6fb3bcbc8
Matheus Felippe Rodrigues da Silva <102191436+matheusfelippe1@users.noreply.github.com>
2022-08-11 21:40:46 -0300

    Add files via upload

---
+++ SwitchCase.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace aula2_1
+{
+    class SwitchCase
+    {
+        public static void Main(string[] args)
+        {
+            string menu "--- Projeto -------\n" +
+                "1 - Fatorial: \n" +
+                "2 - Ver valores de 1 até N \n" +
+                "3 - Sair:";
+            int option = 3;
+            do
+            {
+                Console.WriteLine(menu);
+                option = int.Parse(Console.ReadLine());
+            } while (option != 3);
+        }
+    }
+}
