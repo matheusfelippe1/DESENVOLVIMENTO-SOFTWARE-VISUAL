968583c519282a914279b88803e68e3f6a5f9c59
Matheus Felippe Rodrigues da Silva <102191436+matheusfelippe1@users.noreply.github.com>
2022-09-22 01:11:40 -0300

    Update MotosSonhoController.cs

--- Controller/MotosSonhoController.cs
+++ Controller/MotosSonhoController.cs
@@ -1,30 +1,39 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Motocicleta.Data;
 using Motocicleta.Model;
 
 namespace Motocicleta.Controllers
 {
     [ApiController]
-    [Route("{controller}")]
+    [Route("[Controller]")]
     public class MotosSonhoController : ControllerBase
     {
-        private static List<MotosSonho> MotosSonhos = new List<MotosSonho>();
+        private static List<MotosSonho> MotosSonhosList = new List<MotosSonho>();
+        private readonly Context _context;
+
+        public MotosSonhoController(Context context)
+        {
+            _context = context;
+        }
 
         [HttpPost]
         public void addMotosSonho([FromBody] MotosSonho Foguetao)
         {
-            MotosSonhos.Add(Foguetao);
+            _context.MotoSonho.Add(Foguetao);
+            _context.SaveChanges();
         }
 
         [HttpGet]
         public IActionResult GetMotosSonho()
         {
-            return Ok(MotosSonhos);
+            return Ok(_context.MotoSonho.ToList());
         }
 
         [HttpGet ("{id}")]
-        public IActionResult GetMotosSonhoById(int id)
+        public ActionResult GetMotosSonhoById(int id)
         {
-            MotosSonho motosSonhos = MotosSonhos.FirstOrDefault(motosSonhos => motosSonhos.Id == id);
+            MotosSonho motosSonhos = _context.MotoSonho.FirstOrDefault(motosSonhos => motosSonhos.Id == id);
             if (motosSonhos != null)
             {
                 return Ok(motosSonhos);
@@ -32,6 +41,17 @@ namespace Motocicleta.Controllers
             else
             {
                 return NotFound();
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public void Remove(int id)
+        {
+            var motosSonhos = _context.MotoSonho.FirstOrDefault(motosSonhos => motosSonhos.Id == id);
+            if(motosSonhos != null)
+            {
+                _context.MotoSonho.Remove(motosSonhos);
+                _context.SaveChanges();
             }
         }
     }
